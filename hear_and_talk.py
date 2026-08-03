074e7b76a0290f3403bcacc5b38f29138581df16
Shakmak7866 <shakeebu834@gmail.com>
2026-08-03 11:20:35 -0400

    combined the speechrecog and audio. Now have to simplify code and the assistant hearing its own commands

--- hear_and_talk.py
+++ hear_and_talk.py
@@ -2,6 +2,9 @@ import argparse
 import queue
 import sys
 import sounddevice as sd
+import numpy as np
+from piper import PiperVoice
+from piper.config import SynthesisConfig
 
 from vosk import Model, KaldiRecognizer
 
@@ -47,8 +50,31 @@ args = parser.parse_args(remaining)
 # Add in the actual functions here
 
 def execute_command(command):
-    print(command)
+    model_path = "en_US-arctic-medium.onnx"
+    config_path = "en_US-arctic-medium.onnx.json"
 
+    voice = PiperVoice.load(model_path, config_path=config_path)
+
+    if "hello there" in command.lower():
+        print("recognized")
+        text = "General Kenobi"
+    else:
+        text = "Hello! My name is Arctic from the Piper Voice API. I was built by peter pan"
+
+    syn_config = SynthesisConfig(
+        length_scale = 0.7,
+        speaker_id=6 # 5 6 8 16
+    )
+
+    for chunk in voice.synthesize(text, syn_config=syn_config):
+        audio = np.frombuffer(
+            chunk.audio_int16_bytes,
+            dtype=np.int16
+        )
+        sd.play(audio, voice.config.sample_rate)
+        sd.wait()
+
+    print("Audio Complete")
 
 
 
@@ -79,6 +105,7 @@ try:
             data = q.get()
             if rec.AcceptWaveform(data):
                 command = rec.Result()
+                print(command)
                 execute_command(command)
             #else:
                 #print(rec.PartialResult())
@@ -90,4 +117,5 @@ except KeyboardInterrupt:
     parser.exit(0)
 except Exception as e:
     parser.exit(type(e).__name__ + ": " + str(e))
+
 
